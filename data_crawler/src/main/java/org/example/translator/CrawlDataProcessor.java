f6853b93eaf42c46370ac383ca72210b79171d34
KGW2027 <retryer_@naver.com>
2023-03-25 20:22:09 +0900

    feat: 새로운 번역 및 JSON 정리 모델

--- data_crawler/src/main/java/org/example/translator/CrawlDataProcessor.java
+++ data_crawler/src/main/java/org/example/translator/CrawlDataProcessor.java
@@ -1,5 +1,9 @@
 package org.example.translator;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -8,6 +12,11 @@ import java.io.*;
 import java.util.HashSet;
 
 public abstract class CrawlDataProcessor {
+
+    protected TranslatorMachine translator;
+    public CrawlDataProcessor(TranslatorMachine translator) {
+        this.translator = translator;
+    }
 
     public abstract void process(HashSet<String> texts) throws IOException, ParseException;
     public abstract String getDirectoryName();
@@ -22,5 +31,19 @@ public abstract class CrawlDataProcessor {
         while( (line = br.readLine()) != null) read.append(line);
         br.close();
         return (JSONArray) new JSONParser().parse(read.toString());
+    }
+
+    protected void save(JSONArray result) throws IOException {
+        String target = getSrcPath().replace(".json", "-translated.json");
+        File file = new File(target);
+        if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+        if(!file.exists()) file.createNewFile();
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonElement ge = JsonParser.parseString(result.toJSONString());
+        writer.write(gson.toJson(ge));
+        writer.flush();
+        writer.close();
     }
 }
