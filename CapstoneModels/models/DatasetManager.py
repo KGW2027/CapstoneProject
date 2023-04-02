d0d8b2994dc2536266e08a4da4e76f2418496376
KGW2027 <retryer_@naver.com>
2023-04-02 16:26:02 +0900

    feat: dataset ckpt와 weight 정보를 저장하는 유틸리티

---
+++ CapstoneModels/models/DatasetManager.py
@@ -0,0 +1,40 @@
+import json
+import os.path
+
+
+dir_prefix = 'G:/Dataset-ckpt/ckpt/'
+weight_suffix = '-weight'
+file_suffix = '.json'
+
+def is_exist(name):
+    os.makedirs(dir_prefix, exist_ok=True)
+    path = f'{dir_prefix}{name}{file_suffix}'
+    if os.path.exists(path):
+        return path
+    return None
+
+def load_dataset_ckpt(name):
+    file = is_exist(name)
+    if file is not None:
+        with open(file, 'r', encoding='utf-8') as ckpt_file:
+            return json.load(ckpt_file)
+    return None
+
+def load_dataset_weight(name):
+    file = is_exist(f'{name}{weight_suffix}')
+    if file is not None:
+        with open(file, 'r', encoding='utf-8') as weight_file:
+            return json.load(weight_file)
+    return None
+
+def save_dataset_ckpt(name, value):
+    os.makedirs(dir_prefix, exist_ok=True)
+    path = f'{dir_prefix}{name}{file_suffix}'
+    with open(path, 'w+', encoding='utf-8') as ckpt_file:
+        json.dump(value, ckpt_file, ensure_ascii=False)
+
+def save_dataset_weight(name, value):
+    os.makedirs(dir_prefix, exist_ok=True)
+    path = f'{dir_prefix}{name}{weight_suffix}{file_suffix}'
+    with open(path, 'w+', encoding='utf-8') as weight_file:
+        json.dump(value, weight_file, ensure_ascii=False)
